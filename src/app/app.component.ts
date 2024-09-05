f7f232a6472ef53aceecdde56b04c8b128af690d
iamganesh <ganesh@rhythmflows.com>
2024-09-05 13:34:56 +0530

    unwanted directories are removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule,RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,RouterModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/email-page/email-page.component.ts
+++ src/app/email-page/email-page.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-email-page',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, CommonModule, FormsModule],
   templateUrl: './email-page.component.html',
   styleUrl: './email-page.component.css'
 })
 export class EmailPageComponent {
+  to: string = '';
+  cc: string = '';
+  bcc: string = '';
+  subject: string = '';
+  body: string = '';
+  sendMessage() {
+    // Implement your logic to send the email here
+    console.log('Sending email:', { to: this.to, cc: this.cc, bcc: this.bcc, subject: this.subject, body: this.body });
+  }
 
 }
